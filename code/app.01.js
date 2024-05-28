cfe1b0b2fd9241c927be380680336eeae4277814
Kim MyungSub <audtjqxx@naver.com>
2024-05-28 21:57:05 +0900

    [20240528]: section 3 마무리,  모듈 시스템 (exports, require) 학습

--- code/app.01.js
+++ code/app.01.js
@@ -9,19 +9,52 @@
 
 const http = require("http");
 
-function rqListener(req, res) {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit(); // 이벤트 루프 종료, 서버 종료
+const routes = require("./routes");
+// const fs = require("fs");
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-}
+// function rqListener(req, res) {
+//   //   console.log(req.url, req.method, req.headers);
+//   //   process.exit(); // 이벤트 루프 종료, 서버 종료
 
-const server = http.createServer(rqListener); // 서버생성 시 필수
+//   const url = req.url;
+//   const method = req.method;
+
+//   if (url === "/") {
+//     res.write("<html>");
+//     res.write("<head><title>Enter Message</title></head>");
+//     res.write(
+//       "<body><form action='/message' method='POST'><input type='text/' name='message'><button type='submit'>Send</button></form></body>"
+//     );
+//     res.write("</html>");
+//     return res.end();
+//   }
+
+//   if (url === "/message" && method === "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       console.log(chunk);
+//       body.push(chunk);
+//     });
+//     return req.on("end", () => {
+//       const parsedBody = Buffer.concat(body).toString();
+//       const message = parsedBody.split("=")[1];
+//       fs.writeFileSync("message.txt", message, (err) => {
+//         res.statusCode = 302;
+//         res.setHeader("Location", "/");
+//         return res.end();
+//       });
+//     });
+//   }
+//   res.setHeader("Content-Type", "text/html");
+//   res.write("<html>");
+//   res.write("<head><title>My First Page</title></head>");
+//   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+//   res.write("</html>");
+//   res.end();
+// }
+
+// const server = http.createServer(rqListener); // 서버생성 시 필수
+const server = http.createServer(routes.handler); // 서버생성 시 필수
 http.createServer(function (req, res) {}); // EDA (Event Driven Architecture)
 http.createServer((req, res) => {});
 
