192aaf5383c724ce05f515db0960d3907ebf495d
Kim MyungSub <audtjqxx@naver.com>
2024-05-28 20:35:09 +0900

    [20240528] 3-30, 응답 보내기

--- code/app.01.js
+++ code/app.01.js
@@ -10,8 +10,15 @@
 const http = require("http");
 
 function rqListener(req, res) {
-  console.log(req);
-  process.exit(); // 이벤트 루프 종료, 서버 종료
+  console.log(req.url, req.method, req.headers);
+  //   process.exit(); // 이벤트 루프 종료, 서버 종료
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 }
 
 const server = http.createServer(rqListener); // 서버생성 시 필수
