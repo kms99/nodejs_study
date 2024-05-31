d61adc7735cfe9bd1d7f7a82e1bfd479c7140a01
Kim MyungSub <audtjqxx@naver.com>
2024-05-31 18:21:54 +0900

    [20240531]: 미들웨어 활용 라우팅 학습

--- code/section5/app.js
+++ code/section5/app.js
@@ -4,17 +4,21 @@ const express = require("express");
 
 const app = express();
 
-// middleware 1
-app.use((req, res, next) => {
+// 라우팅 시 1번째 인자는 start path
+app.use("/", (req, res, next) => {
+  console.log("---------------------"); // 항상 실행
+  console.log("Always run!!"); // 항상 실행
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
   console.log("middleware 1");
-  next(); // to next middleware
+  res.send("<h1>Add Product!!</h1>"); // 응답 내용을 자동으로 판단해 제공
 });
 
-// middleware 2
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("middleware 2");
+  res.send("<h1>Hello express!!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
