dad85419ae957a96177f4fbf478c171cbc395119
Kim MyungSub <audtjqxx@naver.com>
2024-05-31 19:03:39 +0900

    [20240531]: express router 모듈 prefix path 지정 학습

--- code/section5/app.js
+++ code/section5/app.js
@@ -10,7 +10,12 @@ const shopRoutes = require("./routes/shop");
 // bodyparser 패키지의 urlencoded는 마지막에 자동으로 next 실행
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes); // 시작 path의 prefix 지정 가능
 app.use(shopRoutes);
+
+//Error 처리
+app.use("/", (req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
--- code/section5/routes/admin.js
+++ code/section5/routes/admin.js
@@ -5,14 +5,14 @@ const router = express.Router();
 router.get("/add-product", (req, res, next) => {
   console.log("middleware 1");
   res.send(`
-      <form action="/product" method="POST">
+      <form action="/admin/add-product" method="POST">
           <input type="text" name="title"/>
           <button type="submit">Add Product</button>
       </form>
     `);
 });
 
-router.post("/product", (req, res) => {
+router.post("/add-product", (req, res) => {
   const product = req.body;
   console.log(product);
   res.redirect("/");
