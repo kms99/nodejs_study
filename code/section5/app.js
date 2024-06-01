a313c227bc0cc99bc9daa262fc45836cbdef533a
Kim MyungSub <audtjqxx@naver.com>
2024-06-01 16:42:42 +0900

    [20240601]: html css 스타일링 및 css 파일 분리(정적파일 허용) 학습

--- code/section5/app.js
+++ code/section5/app.js
@@ -14,6 +14,10 @@ const shopRoutes = require("./routes/shop");
 // bodyparser 패키지의 urlencoded는 마지막에 자동으로 next 실행
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// 정적 파일 (css, 이미지)를 html 파일에서 사용하기 위한 코드
+// public 폴더의 읽기 권한 활성화
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes); // 시작 path의 prefix 지정 가능
 app.use(shopRoutes);
 
